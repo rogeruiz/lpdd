9567befe8cdef733da80194d815deb09d1945beb
Jorge Viramontes <>
2024-08-05 23:15:54 -0500

    Add initial Next.js app

---
+++ src/app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Latiné Professional Development Directory",
+  description:
+    "A directory for organizations and events in the Chicagoland area to support the professional development of Latiné professionals.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
