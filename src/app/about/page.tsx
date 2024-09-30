032a5076d1129685a2b0facf012bc4844a72c32a
Francisco Gonzalez <franciscogonzalez@Franciscos-MacBook-Pro.local>
2024-09-29 22:56:06 -0500

    added a picture to about

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,28 @@
+/*import Image from "next/image";
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-2">
-      About Page
+      Contact Page
     </main>
   );
 }
+
+*/
+
+import Image from 'next/image';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+      <Image 
+        src="/profilepics/Frame 63.jpg" 
+        alt="Frame 63" 
+        width={500} // adjust width as needed
+        height={500} // adjust height as needed
+      />
+    </div>
+  );
+};
+
+export default AboutPage;
