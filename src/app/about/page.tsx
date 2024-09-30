292fe6ec057ba03985af65e6b299a3583d9f6f21
Francisco Gonzalez <franciscogonzalez@Franciscos-MacBook-Pro.local>
2024-09-30 17:50:42 -0500

    created an about page and about.css

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,26 +1,67 @@
-/*import Image from "next/image";
-export default function Page() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-2">
-      Contact Page
-    </main>
-  );
-}
-
-*/
-
 import Image from 'next/image';
+import React from 'react';
+import '../ui/styles/about.css';
+
+const teamMembers = [
+  { 
+    name: "Jorge Viramontes", 
+    role: "Role", 
+    image: "/profilepics/Frame 61.jpg",
+    linkedin: "https://www.linkedin.com/in/jorgealbertoviramontes/"
+  },
+  { 
+    name: "Gerardo Rodriguez", 
+    role: "Role", 
+    image: "/profilepics/Frame 98.jpg",
+    linkedin: "https://www.linkedin.com/in/geraxrodriguez/"
+  },
+  { 
+    name: "Natasha Koller", 
+    role: "Role", 
+    image: "/profilepics/Frame 60.jpg",
+    linkedin: "https://www.linkedin.com"
+  },
+  { 
+    name: "Niza Cuéllar", 
+    role: "Role", 
+    image: "/profilepics/Frame 62.jpg",
+    linkedin: "https://www.linkedin.com/in/niza-cuellar-0a8234188/"
+  },
+  { 
+    name: "Mario Galeno", 
+    role: "Role", 
+    image: "/profilepics/Frame 64.jpg",
+    linkedin: "https://www.linkedin.com"
+  },
+  { 
+    name: "Francisco Gonzalez", 
+    role: "Superman", 
+    image: "/profilepics/Frame 63.jpg",
+    linkedin: "https://www.linkedin.com/in/francisco-gonzalez-0906/"
+  }
+];
 
 const AboutPage = () => {
   return (
-    <div>
-      <h1>About Me</h1>
-      <Image 
-        src="/profilepics/Frame 63.jpg" 
-        alt="Frame 63" 
-        width={500} // adjust width as needed
-        height={500} // adjust height as needed
-      />
+    <div className="about-page">
+      <h2 className="team-heading">Meet Our Team</h2>
+      <div className="team-grid">
+        {teamMembers.map((member, index) => (
+          <div key={index} className="team-member-card">
+            <img src={member.image} alt={member.name} className="team-member-image" />
+            <div className="member-info">
+              {/* Wrapper div for name and LinkedIn icon */}
+              <div className="name-and-linkedin">
+                <h3>{member.name}</h3>
+                <a href={member.linkedin} target="_blank" rel="noopener noreferrer" className="linkedin-icon">
+                  <img src="/profilepics/Frame 69.svg" alt="LinkedIn" className="linkedin-svg-icon" />
+                </a>
+              </div>
+              <p>{member.role}</p>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
