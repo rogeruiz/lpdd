184a109a07b77f031b239a89765af535be776e1b
Gerardo Rodriguez <gerarodriguez@proton.me>
2024-10-07 14:22:19 -0500

    refactor: undid changes in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${lexend.className}`}>
+      <body className={lexend.className}>
         <NavBar />
         {children}
       </body>
